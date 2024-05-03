bfca1c4903fce6bfa94cd873a759e12489ad4b9d
Gennadii <Gennadii_s@inbox.ru>
2024-05-03 22:50:51 +1000

    add code

---
+++ app/users/dependencies.py
@@ -0,0 +1,33 @@
+from fastapi import Depends, Request, HTTPException
+from jose import JWTError, jwt, ExpiredSignatureError
+
+from app.config import settings
+from app.users.dao import UsersDAO
+from app.exceptions import TokenAbsentException, IncorrectTokenFormatException, IncorrectEmailPasswordException, \
+    TokenExpiredException, UserIsNotPresentException
+
+
+def get_token(request: Request):
+    token = request.cookies.get("booking_access_token")
+    if not token:
+        raise TokenAbsentException
+    return token
+
+
+async def get_current_user(token: str = Depends(get_token)):
+    try:
+        payload = jwt.decode(
+            token, settings.SECRET_KEY, settings.ALGORITHM
+        )
+    except ExpiredSignatureError:
+        raise TokenExpiredException
+    except JWTError:
+        raise IncorrectTokenFormatException
+    user_id: str = payload.get("sub")
+    if not user_id:
+        raise UserIsNotPresentException
+    user = await UsersDAO.find_one_or_none(id=int(user_id))
+    if not user:
+        raise UserIsNotPresentException
+
+    return user
