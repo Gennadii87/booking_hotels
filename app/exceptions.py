6ea0fc9b5a1c242bbc45584e7fae1d54946a22b5
Gennadii <Gennadii_s@inbox.ru>
2024-05-03 22:51:36 +1000

    add app/exceptions.py

---
+++ app/exceptions.py
@@ -0,0 +1,39 @@
+from fastapi import HTTPException, status
+
+
+class BookingException(HTTPException):
+    status_code = 500
+    detail = ""
+
+    def __init__(self):
+        super().__init__(status_code=self.status_code, detail=self.detail)
+
+
+class UserAlreadyExistException(BookingException):
+    status_code = status.HTTP_409_CONFLICT
+    detail = "Пользователь уже существует"
+
+
+class IncorrectEmailPasswordException(BookingException):
+    status_code = status.HTTP_401_UNAUTHORIZED
+    detail = "Неверная почта или пароль"
+
+
+class TokenExpiredException(BookingException):
+    status_code = status.HTTP_401_UNAUTHORIZED
+    detail = "Срок действия токена истек"
+
+
+class TokenAbsentException(BookingException):
+    status_code = status.HTTP_401_UNAUTHORIZED
+    detail = "Токен отсутствует"
+
+
+class IncorrectTokenFormatException(BookingException):
+    status_code = status.HTTP_401_UNAUTHORIZED
+    detail = "Токен неверного формата"
+
+
+class UserIsNotPresentException(BookingException):
+    status_code = status.HTTP_401_UNAUTHORIZED
+
